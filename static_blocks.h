5332e17084d3e09d761184d66f600f3074c88a7e
Nolan McCarthy <nolanrmccarthy@gmail.com>
2018-09-11 15:24:12 -0700

    new functionality

--- static_blocks.h
+++ static_blocks.h
@@ -5,16 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BLOCK_SIZE 256 
+#define BLOCK_SIZE 20 
 
 typedef int bool;
 #define true 1
 #define false 0 
 
+
 #define dynamic_block(dtype) struct blocked_##dtype{\
     dtype block[BLOCK_SIZE];\
     struct blocked_##dtype * next;\
     dtype (*get)(struct blocked_##dtype *, int);\
+    dtype * (*get_ptr)(struct blocked_##dtype *, int);\
     void  (*set)(struct blocked_##dtype *, int, dtype el);\
     int length;\
     bool needs_malloc;\
@@ -46,11 +48,20 @@ dtype getitem_##dtype(struct blocked_##dtype * in, int indx){\
         return getitem_##dtype(in->next,indx-BLOCK_SIZE);\
     }\
 }\
+dtype * getitem_ptr_##dtype(struct blocked_##dtype * in, int indx){\
+    if(indx < BLOCK_SIZE){\
+        return &(in->block[indx]);\
+    }\
+    if(in->next!=NULL){\
+        return (getitem_ptr_##dtype(in->next,indx-BLOCK_SIZE));\
+    }\
+}\
 struct blocked_##dtype * new_block_##dtype(void){\
     struct blocked_##dtype * new = (struct blocked_##dtype *)malloc(sizeof(struct blocked_##dtype));\
     new->next = NULL;\
     new->get = getitem_##dtype;\
     new->set = setitem_##dtype;\
+    new->get_ptr = getitem_ptr_##dtype;\
     new->length = 0;\
     new->needs_malloc = false;\
     return new;\
@@ -58,9 +69,11 @@ struct blocked_##dtype * new_block_##dtype(void){\
 \
 
 #define getter(class, ind) (class->get(class,ind))
+#define getter_ptr(class,ind) ((class->get_ptr(class,ind)))
 #define setter(class, ind, el) (class->set(class,ind,el))
 #define swap(class1,class2,ind) (setter(class1,ind,getter(class2,ind)))
 #define len(class) (class->length)
 #define append(class, el) (class->set(class,len(class),el))
+#define last_ptr(class) (getter_ptr(class,(class->length)-1)) 
 
 #endif //__STATIC_BLOCKS_H__
